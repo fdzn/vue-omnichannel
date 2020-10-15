c1f236a4fdc19a84f3ea835a4cc1e9355fb6c87a
Dimas Apriliansyah <dimasapriliansy@gmail.com>
2020-10-15 16:04:06 +0700

    Fix login background

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="form-membership background-clouds">
+  <div style="height: 100vh" class="form-membership background-clouds">
     <div class="form-wrapper">
       <!-- logo -->
       <div class="logo background-yellow">
-        <i class="logo_chat_icon" style="width: 70px !important"></i>
+        <i class="logo_chat_icon" style="width:70px !important"></i>
       </div>
       <!-- ./ logo -->
 
@@ -12,38 +12,61 @@
         Please enter your username and password
       </h6>
       <!-- form -->
-      <form @submit.prevent="login">
+      <form>
         <div class="input-group">
           <div class="input-group-prepend h-42">
-            <span class="input-group-text"><i class="user2_icon"></i></span>
+            <span class="input-group-text" id="inputGroupPrepend"
+              ><i class="user2_icon"></i
+            ></span>
           </div>
           <input
             type="text"
             class="form-control h-form"
+            id="validationCustomUsername"
             placeholder="Username or Email"
-            v-model.trim="username"
+            aria-describedby="inputGroupPrepend"
+            required
           />
-          <!-- <div class="invalid-feedback">Please choose a username.</div> -->
+          <div class="invalid-feedback">
+            Please choose a username.
+          </div>
         </div>
 
         <div class="input-group">
           <div class="input-group-prepend h-42">
-            <span class="input-group-text"><i class="lock2_icon"></i></span>
+            <span class="input-group-text" id="inputGroupPrepend"
+              ><i class="lock2_icon"></i
+            ></span>
           </div>
           <input
             type="password"
             class="form-control h-form"
+            id="validationCustomUsername"
             placeholder="Password"
-            v-model.trim="password"
+            aria-describedby="inputGroupPrepend"
+            required
           />
           <span class="hide_position"><i class="hide_pass_icon"></i></span>
-          <div class="invalid-feedback">Please choose a username.</div>
+          <div class="invalid-feedback">
+            Please choose a username.
+          </div>
         </div>
-
+        <div class="form-group d-flex justify-content-between">
+          <div class="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              class="custom-control-input"
+              checked=""
+              id="customCheck1"
+            />
+            <label class="custom-control-label" for="customCheck1"
+              >Remember me</label
+            >
+          </div>
+        </div>
         <button class="btn btn-blue btn-lg btn-block">Sign in</button>
         <hr />
       </form>
-        <!-- <button class="btn btn-blue btn-lg btn-block" @click="getData">GETDATA</button> -->
       <!-- ./ form -->
     </div>
   </div>
@@ -54,21 +77,27 @@ export default {
   data() {
     return {
       username: "",
-      password: "",
+      password: ""
     };
   },
   methods: {
     async login() {
       const actionPayload = {
         username: this.username,
-        password: this.password,
+        password: this.password
       };
 
       await this.$store.dispatch("login", actionPayload);
-    },
+    }
     // getData(){
     //   this.$store.dispatch("checkLogin");
     // }
-  },
+  }
 };
 </script>
+
+<style scoped>
+/* .form-membership .background-clouds {
+  height: 100vh !important;
+} */
+</style>
