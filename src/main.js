148e3d3c486c0cc7cfbba68a223bb24b606fc822
Akhmad Faudzan Bakri <faudzanbakri@gmail.com>
2020-10-15 15:38:30 +0700

    first dev socket

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from './store'
-// import store from "./store/index.js";
+import store from "./store";
+// import socketio from "socket.io";
+import VueSocketIO from "vue-socket.io";
 Vue.config.productionTip = false;
+
+//SOCKET
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: process.env.VUE_APP_URL_SOCKET,
+    vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+    },
+}))
 
 new Vue({
   router,
   store,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
